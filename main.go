da19e88169f5670dc7a81a28a89bde4f284a42b6
tsthght <tsthght@163.com>
2020-05-04 23:16:42 +0800

    init

---
+++ main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"net"
+	"os"
+	"errors"
+
+	"github.com/tsthght/s3folder/s3common/s3castleclient"
+)
+
+func newCastleClient( ) (castleManager *s3castleclient.CastleClientManager, err error) {
+	var ip, host string
+	err, ip = GetLocalIP()
+	if err != nil {
+		return nil, err
+	}
+	err, host = GetLocalHost()
+	if err != nil {
+		return nil, err
+	}
+	if castleManager, err = s3castleclient.NewCastleClientManager(host, ip,
+		"", "", ""); err != nil {
+		return nil, err
+	}
+	return
+}
+
+func main() {
+	newCastleClient()
+}
+
+func GetLocalIP() (error, string) {
+	addrs, err := net.InterfaceAddrs()
+
+	if err != nil {
+		return err, ""
+	}
+
+	for _, address := range addrs {
+		if ipnet, ok := address.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
+			if ipnet.IP.To4() != nil {
+				return nil, ipnet.IP.String()
+			}
+		}
+	}
+	return errors.New("can not get local ip"), ""
+}
+
+func GetLocalHost() (error, string) {
+	host, err := os.Hostname()
+	if err != nil {
+		return err, ""
+	}
+	return nil, host
+}
